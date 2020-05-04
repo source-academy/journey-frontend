128b1d7469874e72cd52886a2bf9aa7f4f8f013e
chuyiting <59797323+chuyiting@users.noreply.github.com>
2020-05-04 12:41:00 +0800

    Updates

---
+++ externalLibs/index.js
@@ -0,0 +1,94 @@
+/**
+ * Load a given external library, as a javascript file
+ * to run in the global scope, by adding it to the DOM
+ */
+function dynamicallyLoadScript(url) {
+  var script = document.createElement('script')
+  script.src = url
+  /** Forces scripts to be loaded in order. */
+  script.async = false
+  script.defer = true
+  document.head.appendChild(script)
+}
+
+/**
+ * Loads all libraries, including sound and graphics.
+ */
+function loadAllLibs() {
+  const files = [
+    // list library
+    '/externalLibs/list.js',
+    // sound
+    '/externalLibs/sound/sounds.js',
+    '/externalLibs/sound/soundToneMatrix.js',
+    '/externalLibs/sound/riffwave.js',
+    '/externalLibs/sound/microphone.js',
+    // graphics
+    '/externalLibs/graphics/gl-matrix.js',
+    '/externalLibs/graphics/webGLhi_graph.js',
+    '/externalLibs/graphics/webGLhi_graph_ce.js',
+    '/externalLibs/graphics/webGLgraphics.js',
+    '/externalLibs/graphics/webGLcurve.js',
+    '/externalLibs/graphics/webGLrune.js',
+    // list visualizer
+    '/externalLibs/visualizer/KineticJS.js',
+    '/externalLibs/visualizer/visualizer.js',
+    // binary tree library
+    '/externalLibs/tree.js',
+    // support for Practical Assessments (presently none)
+    // video
+    '/externalLibs/video/video_lib.js',
+    // inspector
+    '/externalLibs/inspector/inspector.js',
+    // env visualizer
+    '/externalLibs/env_visualizer/ConcreteJs.js',
+    '/externalLibs/env_visualizer/visualizer.js'
+  ]
+
+  for (var i = 0; i < files.length; i++) {
+    dynamicallyLoadScript(files[i])
+  }
+}
+
+/**
+ * Loads libraries according to the name provided.
+ * This is to faciliate a lack of namespace clash for
+ * graphics libraries (@see #341)
+ */
+function loadLib(externalLibraryName) {
+  let files
+  switch (externalLibraryName) {
+    case 'RUNES':
+      files = [
+        // graphics
+        '/externalLibs/graphics/gl-matrix.js',
+        '/externalLibs/graphics/webGLgraphics.js',
+        '/externalLibs/graphics/webGLrune.js'
+      ]
+      break
+    case 'CURVES':
+      files = [
+        // graphics
+        '/externalLibs/graphics/gl-matrix.js',
+        '/externalLibs/graphics/webGLhi_graph.js',
+        '/externalLibs/graphics/webGLhi_graph_ce.js',
+        '/externalLibs/graphics/webGLgraphics.js',
+        '/externalLibs/graphics/webGLcurve.js'
+      ]
+      break
+      case 'MACHINELEARNING':
+      files = [
+        // faceAPI
+        '/externalLibs/faceapi/face-api_last_version.min.js',
+        '/externalLibs/faceapi/faceapi.js',
+      ]
+      break
+    default:
+      break
+  }
+  for (var i = 0; i < files.length; i++) {
+    dynamicallyLoadScript(files[i])
+  }
+}
+
+loadAllLibs()
---
+++ precache-manifest.fd5f6198b3cafea22d44858793c7bfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779f46a88be9cf18ccbfbed3b66681ed",
+    "url": "/journey-frontend/index.html"
+  },
+  {
+    "revision": "a0dffeb82385f030c052",
+    "url": "/journey-frontend/static/css/main.9be274f3.chunk.css"
+  },
+  {
+    "revision": "29ce647ad7468c196e74",
+    "url": "/journey-frontend/static/js/2.6258ff5e.chunk.js"
+  },
+  {
+    "revision": "0ee26d84ea1ac2dbc7154924b43dbbd9",
+    "url": "/journey-frontend/static/js/2.6258ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0dffeb82385f030c052",
+    "url": "/journey-frontend/static/js/main.3146b7c0.chunk.js"
+  },
+  {
+    "revision": "4231d3d4a4b396a64c67",
+    "url": "/journey-frontend/static/js/runtime-main.34d4f54b.js"
+  }
+]);
